9ce624d894eb21b7e590a66f7e6f930853e295ed
akifumi_um5 <10093316+coma2010@users.noreply.github.com>
2025-11-01 15:59:41 +0900

    fix 251101 4

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/user/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/user/keymap.c
@@ -28,6 +28,7 @@ const uint16_t PROGMEM my_hj[] = {KC_H, KC_J, COMBO_END};
 const uint16_t PROGMEM my_jk[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM my_kl[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM my_msc[] = {KC_M, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM my_codot[] = {KC_COMMA, KC_DOT, COMBO_END};
 const uint16_t PROGMEM my_du[] = {KC_D, KC_U, COMBO_END};
 const uint16_t PROGMEM my_dj[] = {KC_D, KC_J, COMBO_END};
 const uint16_t PROGMEM my_dm[] = {KC_D, KC_M, COMBO_END};
@@ -42,12 +43,13 @@ combo_t key_combos[] = {
     [RIGHT] = COMBO(my_jk, KC_RGHT),
     [MOUSE1] = COMBO(my_kl, KC_BTN1),
     [MOUSE2] = COMBO(my_msc, KC_BTN2),
+    [KEY7] = COMBO(my_codot, SCRL_MO),
     [S8] = COMBO(my_du, S(KC_8)),
     [S9] = COMBO(my_du, S(KC_9)),
     [LSBRC] = COMBO(my_dj, S(KC_LBRC)),
     [RSBRC] = COMBO(my_dk, S(KC_RBRC)),
     [LBRC] = COMBO(my_dm, KC_LBRC),
-    [RBRC] = COMBO(my_dm, KC_RBRC),
+    [RBRC] = COMBO(my_dsc, KC_RBRC),
 }
 
 // enum combos
