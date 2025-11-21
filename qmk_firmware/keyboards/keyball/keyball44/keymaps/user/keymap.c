d3827fe3660d3121946c35dd1dbe0cc877c571d1
akifumi_um5 <10093316+coma2010@users.noreply.github.com>
2025-11-21 17:37:37 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/user/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/user/keymap.c
@@ -90,49 +90,61 @@ enum custom_keycodes
   D_ARW
 };
 
+// Tap Dance declarations
+enum
+{
+  TD_Q_ESC,
+}
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // keymap for default (VIA)
     [_DEFAULT] = LAYOUT_universal(
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         LSFT_T(KC_TAB), LSFT_T(KC_A), LT(6, KC_S), LT(3, KC_D), LT(2, KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, RSFT_T(KC_MINS), LT(6, KC_ENT),
         LCTL_T(KC_CAPS), LCTL_T(KC_Z), KC_X, KC_C, LT(5, KC_V), KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RCTL_T(KC_SLSH), S(KC_INT1),
-        KC_LALT, KC_LGUI, LCTL_T(KC_TAB), KC_LNG8, LT(5, KC_ESC), LSFT_T(KC_BSPC), LT(1, KC_SPC), TG(5), KC_RCTL, KC_RALT),
+        KC_LALT, KC_LGUI, LCTL_T(KC_TAB), KC_LNG8, LT(5, KC_ESC), LSFT_T(KC_BSPC), LT(1, KC_SPC), TG(5), KC_RCTL, TG(_MOUSE)),
 
     [_NUMBER] = LAYOUT_universal(
         _______, S(KC_1), KC_LBRC, S(KC_3), S(KC_4), S(KC_5), KC_EQL, S(KC_6), S(JP_COLN), KC_MINS, S(KC_MINS), _______,
         _______, LSFT_T(KC_1), LT(6, KC_2), LT(3, KC_3), LT(2, KC_4), KC_5, KC_6, KC_7, KC_8, KC_9, RSFT_T(KC_0), _______,
         _______, LCTL_T(JP_LBRC), JP_RBRC, S(JP_LBRC), S(JP_RBRC), JP_COLN, S(JP_CIRC), S(JP_SCLN), _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, TG(_NUMBER), _______, _______),
+        _______, _______, _______, _______, _______, _______, _______, TG(_NUMBER), _______, TG(_NUMBER)),
 
     [_BRACKET] = LAYOUT_universal(
-        _______, XXXXXXX, D_ARW, S_ARW, XXXXXXX, XXXXXXX, S(KC_8), S(KC_9), S(KC_2), S(KC_7), S(JP_AT), _______,
+        _______, TD(TD_Q_ESC), D_ARW, S_ARW, XXXXXXX, XXXXXXX, S(JP_YEN), S(KC_8), S(KC_9), S(KC_2), S(KC_7), _______,
         _______, KC_DEL, LT(6, KC_PGUP), LT(3, KC_HOME), XXXXXXX, XXXXXXX, S(JP_BSLS), S(JP_LBRC), S(JP_RBRC), JP_SCLN, JP_COLN, _______,
-        _______, KC_BSPC, KC_PGDN, KC_END, XXXXXXX, XXXXXXX, XXXXXXX, JP_LBRC, JP_RBRC, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, TG(_BRACKET), _______, _______),
+        _______, KC_BSPC, KC_PGDN, KC_END, XXXXXXX, XXXXXXX, XXXXXXX, JP_LBRC, JP_RBRC, S(JP_AT), _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, TG(_BRACKET), _______, TG(_BRACKET)),
 
     [_FUNCTION] = LAYOUT_universal(
         _______, KC_F1, KC_F2, _______, KC_F3, KC_F4, _______, _______, _______, _______, _______, _______,
         _______, KC_F5, KC_F6, _______, KC_F7, KC_F8, _______, S_ARW, D_ARW, _______, _______, _______,
         _______, KC_F9, KC_F10, _______, KC_F11, KC_F12, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, TG(_FUNCTION), _______, _______),
+        _______, _______, _______, _______, _______, _______, _______, TG(_FUNCTION), _______, TG(_FUNCTION)),
 
     [_CURSOR] = LAYOUT_universal(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, TG(_CURSOR), _______, _______),
+        _______, _______, _______, _______, _______, _______, _______, TG(_CURSOR), _______, TG(_CURSOR)),
 
     [_MOUSE] = LAYOUT_universal(
         _______, _______, KC_HOME, KC_UP, KC_PGUP, KC_DEL, TO(0), TG(3), _______, _______, _______, _______,
         _______, KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS, TG(1), TG(4), _______, _______, _______, _______,
         _______, KC_LCTL, KC_END, KC_DOWN, KC_PGDN, _______, TG(2), TG(6), _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, TG(_MOUSE), _______, _______),
+        _______, _______, _______, _______, _______, _______, _______, TG(_MOUSE), _______, TG(_MOUSE)),
 
     [_MISC] = LAYOUT_universal(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, TG(_MISC), _______, _______),
+        _______, _______, _______, _______, _______, _______, _______, TG(_MISC), _______, TG(_MISC)),
 };
 // clang-format on
 
