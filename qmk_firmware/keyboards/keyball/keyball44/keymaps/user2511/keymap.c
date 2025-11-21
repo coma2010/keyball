325f479c484f02584e9d7a8ffb30cfbe05f15860
akifumi_um5 <10093316+coma2010@users.noreply.github.com>
2025-11-21 23:12:45 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/user2511/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/user2511/keymap.c
@@ -25,15 +25,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // combo setting
 // #ifdef COMBO_ENABLE
-const uint16_t PROGMEM my_up[] = {KC_Y, KC_U, COMBO_END};
-const uint16_t PROGMEM my_down[] = {KC_N, KC_M, COMBO_END};
+const uint16_t PROGMEM my_up[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM my_down[] = {KC_M, KC_COMM, COMBO_END};
 const uint16_t PROGMEM my_left[] = {KC_H, KC_J, COMBO_END};
 const uint16_t PROGMEM my_right[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM my_btn1[] = {KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM my_btn2[] = {KC_L, RSFT_T(KC_MINS), COMBO_END};
-const uint16_t PROGMEM my_scrl_mo[] = {KC_COMM, KC_DOT, COMBO_END};
-const uint16_t PROGMEM my_esc[] = {KC_U, KC_I, COMBO_END};
-const uint16_t PROGMEM my_tab[] = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM my_btn2[] = {KC_COMMA, KC_DOT, COMBO_END};
+const uint16_t PROGMEM my_scrl_mo[] = {KC_DOT, KC_SLSH, COMBO_END};
+const uint16_t PROGMEM my_lprn[] = {KC_D, KC_U, COMBO_END};
+const uint16_t PROGMEM my_rprn[] = {KC_D, KC_I, COMBO_END};
+const uint16_t PROGMEM my_lcbr[] = {KC_D, KC_J, COMBO_END};
+const uint16_t PROGMEM my_rcbr[] = {KC_D, KC_K, COMBO_END};
+const uint16_t PROGMEM my_lbrc[] = {KC_D, KC_M, COMBO_END};
+const uint16_t PROGMEM my_rbrc[] = {KC_D, KC_COMMA, COMBO_END};
+const uint16_t PROGMEM my_dquo[] = {KC_D, KC_O, COMBO_END};
+const uint16_t PROGMEM my_quot[] = {KC_D, KC_L, COMBO_END};
+const uint16_t PROGMEM my_esc[] = {KC_Q, KC_W, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(my_up, KC_UP),
@@ -43,8 +50,15 @@ combo_t key_combos[] = {
     COMBO(my_btn1, KC_BTN1),
     COMBO(my_btn2, KC_BTN2),
     COMBO(my_scrl_mo, SCRL_MO),
+    COMBO(my_lprn, S(KC_8)),
+    COMBO(my_rprn, S(KC_9)),
+    COMBO(my_lcbr, S(KC_RBRC)),
+    COMBO(my_rcbr, S(KC_NUHS)),
+    COMBO(my_lbrc, KC_RBRC),
+    COMBO(my_rbrc, KC_NUHS),
+    COMBO(my_dquo, S(KC_2)),
+    COMBO(my_quot, S(KC_7)),
     COMBO(my_esc, KC_ESC),
-    COMBO(my_tab, KC_TAB),
 };
 
 // # define OVR_TGL KEY_OVERRIDE_TOGGLE
@@ -65,6 +79,7 @@ enum layer_number
   _NUMBER,
   _BRACKET,
   _FUNCTION,
+  _CURSOR,
   _MOUSE,
   _MISC
 };
@@ -75,107 +90,48 @@ enum custom_keycodes
   D_ARW
 };
 
-bool get_retro_tapping(uint16_t keycode, keyrecord_t *record)
-{
-  switch (keycode)
-  {
-  case LCTL_T(KC_Z):
-  case LSFT_T(KC_A):
-  case LT(_MISC, KC_S):
-    return true;
-  default:
-    return false;
-  }
-}
-
-// Tap Dance declarations
-enum
-{
-  TD_Q_ESC,
-  TD_LPRIN,
-  TD_RPRIN,
-};
-
-void dance_lprin(tap_dance_state_t *state, void *user_date)
-{
-  switch (state->count)
-  {
-  case 1:
-    register_code(KC_LSFT);
-    tap_code(KC_8);
-    unregister_code(KC_LSFT);
-  case 2:
-    tap_code(JP_LBRC);
-  case 3:
-    register_code(KC_LSFT);
-    tap_code(JP_LBRC);
-    unregister_code(KC_LSFT);
-  default:
-    tap_code(JP_LBRC);
-  }
-}
-
-void dance_rprin(tap_dance_state_t *state, void *user_date)
-{
-  switch (state->count)
-  {
-  case 1:
-    register_code(KC_LSFT);
-    tap_code(KC_9);
-    unregister_code(KC_LSFT);
-  case 2:
-    tap_code(JP_RBRC);
-  case 3:
-    register_code(KC_LSFT);
-    tap_code(JP_RBRC);
-    unregister_code(KC_LSFT);
-  default:
-    tap_code(JP_RBRC);
-  }
-}
-
-tap_dance_action_t tap_dance_actions[] = {
-    [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
-    [TD_LPRIN] = ACTION_TAP_DANCE_FN(dance_lprin),
-    [TD_RPRIN] = ACTION_TAP_DANCE_FN(dance_rprin),
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // keymap for default (VIA)
     [_DEFAULT] = LAYOUT_universal(
-        KC_ESC, TD(TD_Q_ESC), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        LSFT_T(KC_TAB), LSFT_T(KC_A), LT(_MISC, KC_S), LT(_FUNCTION, KC_D), LT(_BRACKET, KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, RSFT_T(KC_MINS), LT(_MISC, KC_ENT),
-        LCTL_T(KC_CAPS), LCTL_T(KC_Z), KC_X, KC_C, LT(_MOUSE, KC_V), KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RCTL_T(KC_SLSH), S(KC_INT1),
-        KC_LALT, KC_LGUI, LCTL_T(KC_TAB), KC_LNG8, LT(_MOUSE, KC_ESC), LSFT_T(KC_BSPC), LT(_NUMBER, KC_SPC), TG(_MOUSE), KC_RCTL, TG(_MOUSE)),
+        KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+        LSFT_T(KC_TAB), LSFT_T(KC_A), LT(6, KC_S), LT(3, KC_D), LT(2, KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, RSFT_T(KC_MINS), LT(6, KC_ENT),
+        LCTL_T(KC_CAPS), LCTL_T(KC_Z), KC_X, KC_C, LT(5, KC_V), KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RCTL_T(KC_SLSH), S(KC_INT1),
+        KC_LALT, KC_LGUI, LCTL_T(KC_TAB), KC_LNG8, LT(5, KC_ESC), LSFT_T(KC_BSPC), LT(1, KC_SPC), TG(5), KC_RCTL, TG(_MOUSE)),
 
     [_NUMBER] = LAYOUT_universal(
         _______, S(KC_1), KC_LBRC, S(KC_3), S(KC_4), S(KC_5), KC_EQL, S(KC_6), S(JP_COLN), KC_MINS, S(KC_MINS), _______,
-        _______, LSFT_T(KC_1), LT(_MOUSE, KC_2), LT(_FUNCTION, KC_3), LT(_BRACKET, KC_4), KC_5, KC_6, KC_7, KC_8, KC_9, RSFT_T(KC_0), _______,
+        _______, LSFT_T(KC_1), LT(6, KC_2), LT(3, KC_3), LT(2, KC_4), KC_5, KC_6, KC_7, KC_8, KC_9, RSFT_T(KC_0), _______,
         _______, LCTL_T(JP_LBRC), JP_RBRC, S(JP_LBRC), S(JP_RBRC), JP_COLN, S(JP_CIRC), S(JP_SCLN), _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, TG(_NUMBER), _______, TG(_NUMBER)),
 
     [_BRACKET] = LAYOUT_universal(
-        _______, TD(TD_Q_ESC), D_ARW, S_ARW, XXXXXXX, XXXXXXX, S(JP_YEN), S(KC_8), S(KC_9), S(KC_2), S(KC_7), _______,
-        _______, KC_DEL, LT(_MISC, KC_PGUP), LT(_FUNCTION, KC_HOME), XXXXXXX, XXXXXXX, S(JP_BSLS), S(JP_LBRC), S(JP_RBRC), JP_SCLN, JP_COLN, _______,
+        _______, _______, D_ARW, S_ARW, XXXXXXX, XXXXXXX, S(JP_YEN), S(KC_8), S(KC_9), S(KC_2), S(KC_7), _______,
+        _______, KC_DEL, LT(6, KC_PGUP), LT(3, KC_HOME), XXXXXXX, XXXXXXX, S(JP_BSLS), S(JP_LBRC), S(JP_RBRC), JP_SCLN, JP_COLN, _______,
         _______, KC_BSPC, KC_PGDN, KC_END, XXXXXXX, XXXXXXX, XXXXXXX, JP_LBRC, JP_RBRC, S(JP_AT), _______, _______,
         _______, _______, _______, _______, _______, _______, _______, TG(_BRACKET), _______, TG(_BRACKET)),
 
     [_FUNCTION] = LAYOUT_universal(
-        _______, KC_F1, KC_F2, XXXXXXX, KC_F3, KC_F4, _______, TD(TD_LPRIN), TD(TD_RPRIN), _______, _______, _______,
-        _______, KC_F5, LT(_MISC, KC_F6), XXXXXXX, KC_F7, KC_F8, _______, S_ARW, D_ARW, _______, _______, _______,
-        _______, KC_F9, KC_F10, XXXXXXX, KC_F11, KC_F12, _______, _______, _______, _______, _______, _______,
+        _______, KC_F1, KC_F2, _______, KC_F3, KC_F4, _______, _______, _______, _______, _______, _______,
+        _______, KC_F5, KC_F6, _______, KC_F7, KC_F8, _______, S_ARW, D_ARW, _______, _______, _______,
+        _______, KC_F9, KC_F10, _______, KC_F11, KC_F12, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, TG(_FUNCTION), _______, TG(_FUNCTION)),
 
+    [_CURSOR] = LAYOUT_universal(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, TG(_CURSOR), _______, TG(_CURSOR)),
+
     [_MOUSE] = LAYOUT_universal(
-        _______, _______, KC_HOME, KC_UP, KC_PGUP, KC_DEL, TO(_DEFAULT), TG(_FUNCTION), _______, _______, _______, _______,
-        _______, KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS, TG(_NUMBER), _______, KC_BTN1, KC_BTN2, SCRL_MO, _______,
-        _______, KC_LCTL, KC_END, KC_DOWN, KC_PGDN, _______, TG(_BRACKET), TG(_MISC), KC_BTN3, KC_BTN4, KC_BTN5, _______,
+        _______, _______, KC_HOME, KC_UP, KC_PGUP, KC_DEL, TO(0), TG(3), _______, _______, _______, _______,
+        _______, KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS, TG(1), TG(4), _______, _______, _______, _______,
+        _______, KC_LCTL, KC_END, KC_DOWN, KC_PGDN, _______, TG(2), TG(6), _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, TG(_MOUSE), _______, TG(_MOUSE)),
 
     [_MISC] = LAYOUT_universal(
-        _______, _______, _______, _______, _______, _______, _______, _______, SSNP_FRE, _______, _______, _______,
-        _______, _______, _______, CPI_I1K, CPI_I100, _______, _______, SCRL_DVI, SSNP_VRT, KBC_RST, _______, _______,
-        _______, _______, _______, CPI_D1K, CPI_D100, _______, _______, SCRL_DVD, SSNP_HOR, KBC_SAVE, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, TG(_MISC), _______, TG(_MISC)),
 };
 // clang-format on
@@ -183,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state)
 {
   // Auto enable scroll mode when the highest layer is 3
-  keyball_set_scroll_mode(get_highest_layer(state) == _MISC);
+  keyball_set_scroll_mode(get_highest_layer(state) == 6);
   return state;
 }
 
@@ -249,6 +205,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
     }
     return false;
     break;
+  // case MO6_USCR:
+  //   if (record->event.pressed)
+  //   {
+
+  //   }
   case S_ARW:
     if (record->event.pressed)
     {
