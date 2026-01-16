8a2b151942689a2cf94f23510adf0d1e5cc69932
akifumi_aix1 <10093316+coma2010@users.noreply.github.com>
2026-01-16 17:27:04 +0900

    fix

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/user/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/user/keymap.c
@@ -496,7 +496,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT_universal(
         // 基本レイヤー: 文字入力の主軸。Tap/Holdやレイヤー移動を多用する。
         KC_ESC, TD(TD_Q_ESC), TD(TD_W_TAB), KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        LSFT_T(KC_CAPS), LSFT_T(KC_A), KC_S, LT(_MOUSE, KC_D), LT(_BRACKET, KC_F), LT(_FUNCTION, KC_G), KC_H, KC_J, KC_K, KC_L, RSFT_T(KC_MINS), LT(_MISC, KC_ENT),
+        LSFT_T(KC_CAPS), LSFT_T(KC_A), KC_S, LT(_MOUSE, KC_D), LT(_BRACKET, KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, RSFT_T(KC_MINS), LT(_MISC, KC_ENT),
         LCTL_T(KC_LNG1), LCTL_T(KC_Z), KC_X, KC_C, LT(_MISC, KC_V), KC_B, KC_N, KC_M, KC_COMM, KC_DOT, LCTL_T(KC_SLSH), S(KC_INT1),
         // KC_LALT, KC_LGUI, CTL_USCR, KC_LNG8, LT(_NUMBER, KC_TAB), LSFT_T(KC_BSPC), LT(_NUMBER, KC_SPC), TG(_MOUSE), KC_LCTL, TG(_MOUSE)),
         S(JP_BSLS), KC_LALT, LCTL_T(KC_ESC), KC_LNG7, LGUI_T(KC_ENT), LSFT_T(KC_ENT), LT(_NUMBER, KC_SPC), TG(_MOUSE), KC_LCTL, TG(_MOUSE)),
@@ -507,7 +507,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // _______, LSFT_T(KC_1), LT(_MOUSE, KC_2), LT(_FUNCTION, KC_3), LT(_BRACKET, KC_4), KC_5, KC_6, KC_7, KC_8, KC_9, RSFT_T(KC_0), _______,
         // _______, LCTL_T(JP_LBRC), TD(TD_QUOT), TD(TD_LBRC), TD(TD_RBRC), KC_MINS, S(JP_CIRC), S(JP_SCLN), _______, _______, _______, _______,
         _______, S(KC_1), JP_AT, S(KC_3), S(KC_4), S(KC_5), TD(TD_QUOT), TD(TD_LBRC), TD(TD_RBRC), S(KC_8), S(KC_9), _______,
-        _______, LSFT_T(KC_1), KC_2, LT(_MOUSE, KC_3), LT(_BRACKET, KC_4), LT(_FUNCTION, KC_5), TD(TD_6), TD(TD_7), TD(TD_8), TD(TD_9), TD(TD_00), _______,
+        _______, LSFT_T(KC_1), KC_2, LT(_MOUSE, KC_3), LT(_BRACKET, KC_4), KC_5, TD(TD_6), TD(TD_7), TD(TD_8), TD(TD_9), TD(TD_00), _______,
         _______, LCTL_T(JP_BSLS), S(JP_AT), S(JP_CIRC), S(JP_BSLS), S(JP_YEN), S(JP_SCLN), KC_MINS, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, TG(_NUMBER), _______, TG(_NUMBER)),
 
